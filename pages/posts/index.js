96a2cce210bf60c1cb93123c786b670abc2f2543
Vaibhav Chakane <vaibhavchakane@Vaibhavs-MacBook-Air.local>
2023-11-01 19:07:21 +0530

    show all posts on all posts route

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import AllPosts from "../../components/posts/all-posts";
 
-const AllPosts = () => {
-  return (
-    <div>AllPosts</div>
-  )
-}
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting Started with NextJS",
+    image: "getting-started-with-nextjs.png",
+    excerpt:
+      "Nextjs is a React framework for production and is the only one that is available to be there to build the apps that are production ready.",
+    date: "2023-03-10",
+  },
+  {
+    slug: "getting-started-with-nextjs-2",
+    title: "Getting Started with NextJS",
+    image: "getting-started-with-nextjs.png",
+    excerpt:
+      "Nextjs is a React framework for production and is the only one that is available to be there to build the apps that are production ready.",
+    date: "2023-03-10",
+  },
+  {
+    slug: "getting-started-with-nextjs-3",
+    title: "Getting Started with NextJS",
+    image: "getting-started-with-nextjs-3.png",
+    excerpt:
+      "Nextjs is a React framework for production and is the only one that is available to be there to build the apps that are production ready.",
+    date: "2023-03-10",
+  },
+  {
+    slug: "getting-started-with-nextjs-4",
+    title: "Getting Started with NextJS",
+    image: "getting-started-with-nextjs-4.png",
+    excerpt:
+      "Nextjs is a React framework for production and is the only one that is available to be there to build the apps that are production ready.",
+    date: "2023-03-10",
+  },
+];
 
-export default AllPosts
+const EveryPosts = () => {
+  return <AllPosts posts={DUMMY_POSTS} />;
+};
+
+export default EveryPosts;

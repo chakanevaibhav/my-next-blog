b17af3f252408d900a9884c4c4f5a994865087a9
Vaibhav Chakane <vaibhavchakane@Vaibhavs-MacBook-Air.local>
2023-11-01 18:14:56 +0530

    added post item component

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -1,7 +1,28 @@
 import React from "react";
+import classes from "./post-item.module.css";
 
-const PostItem = () => {
-  return <div>PostItem</div>;
+const PostItem = (props) => {
+  const { title, image, excerpt, date, slug } = props.post;
+  const formatedDate = new Date(date).toLocaleDateString('en-Us',{
+    day:'numeric',
+    month:'long',
+    year:'numeric'
+  })
+  const imagePath=`/images/posts/${slug}/${image}`
+  return (
+    <li className={classes.post}>
+      <Link>
+        <div className={classes.image}>
+          <Image src={imagePath} alt={title} width={300} height={200}/>
+        </div>
+        <div className={classes.content}>
+          <h3>{title}</h3>
+          <time>{formatedDate}</time>
+          <p>{excerpt}</p>
+        </div>
+      </Link>
+    </li>
+  );
 };
 
 export default PostItem;

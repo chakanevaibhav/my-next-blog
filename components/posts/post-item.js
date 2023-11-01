91820fc9f5a05a8cca06fe8fbe25e80a9d0de413
Vaibhav Chakane <vaibhavchakane@Vaibhavs-MacBook-Air.local>
2023-11-01 18:52:13 +0530

    make image responsive

--- components/posts/post-item.js
+++ components/posts/post-item.js
@@ -5,18 +5,24 @@ import Image from "next/image";
 
 const PostItem = (props) => {
   const { title, image, excerpt, date, slug } = props.post;
-  const formatedDate = new Date(date).toLocaleDateString('en-Us',{
-    day:'numeric',
-    month:'long',
-    year:'numeric'
-  })
-  const imagePath=`/images/posts/${slug}/${image}`
-  const linkPath = `/posts/${slug}`
+  const formatedDate = new Date(date).toLocaleDateString("en-Us", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+  });
+  const imagePath = `/images/posts/${slug}/${image}`;
+  const linkPath = `/posts/${slug}`;
   return (
     <li className={classes.post}>
       <Link href={linkPath}>
         <div className={classes.image}>
-          <Image src={imagePath} alt={title} width={300} height={200}/>
+          <Image
+            src={imagePath}
+            alt={title}
+            width={300}
+            height={200}
+            layout="responsive"
+          />
         </div>
         <div className={classes.content}>
           <h3>{title}</h3>

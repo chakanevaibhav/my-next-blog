7ee8a8e76c4ba2e46667579eb8a77d89a462402f
Vaibhav Chakane <vaibhavchakane@Vaibhavs-MacBook-Air.local>
2023-11-01 16:43:21 +0530

    added hero component

--- components/home/hero.js
+++ components/home/hero.js
@@ -1,8 +1,17 @@
-import React from 'react'
+import classes from './hero.module.scss'
+import Image from 'next/image'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className={classes.hero}>
+        <div className={classes.image}>
+            <Image src="/images/site/corporate-guy-profile-image.avif" alt="An Image showing Me" width={360} height={360} />
+        </div>
+        <h1>Hi, I am Vaibhav</h1>
+        <p>
+            I am a web developer specialized in web development using frontend framework like next.js, react.js along with Javascript, HTML, CSS etc.
+        </p>
+    </section>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
+import FeaturedPosts from "../components/home/featured-posts"
+import Hero from "../components/home/hero"
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <Hero />
+    <FeaturedPosts />
+    </>
   )
 }
 

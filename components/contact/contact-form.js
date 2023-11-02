c95b87d95819820ec08de85e274ff7fb0566b498
Vaibhav Chakane <vaibhavchakane@Vaibhavs-MacBook-Air.local>
2023-11-02 19:03:57 +0530

    adding success notification

--- components/contact/contact-form.js
+++ components/contact/contact-form.js
@@ -1,14 +1,16 @@
 import React, { useState } from "react";
 import classes from "./contact-form.module.css";
+import Notification from "../ui/notification";
 
 const ContactForm = () => {
   const [enteredEmail, setEnteredEmail] = useState("");
   const [enteredName, setEnteredName] = useState("");
   const [enteredMessage, setEnteredMessage] = useState("");
+  const [success, setSuccess] = useState(false);
 
-  const sendMessageHandler = (event) => {
+  const sendMessageHandler = async (event) => {
     event.preventDefault();
-    fetch("/api/contact", {
+    await fetch("/api/contact", {
       method: "POST",
       body: JSON.stringify({
         email: enteredEmail,
@@ -19,48 +21,58 @@ const ContactForm = () => {
         "Content-Type": "application/json", // Corrected "Content-Type"
       },
     });
+    setSuccess(true);
   };
 
   return (
-    <section className={classes.contact}>
-      <h1>How can I help you?</h1>
-      <form className={classes.form} onSubmit={sendMessageHandler}>
-        <div className={classes.controls}>
-          <div className={classes.control}>
-            <label htmlFor="email">Your email</label>
-            <input
-              type="email"
-              id="email"
-              required
-              value={enteredEmail}
-              onChange={(e) => setEnteredEmail(e.target.value)}
-            />
+    <>
+      {success && (
+        <Notification
+          title="Success"
+          message="Details submitted sucessfully"
+          status="success"
+        />
+      )}
+      <section className={classes.contact}>
+        <h1>How can I help you?</h1>
+        <form className={classes.form} onSubmit={sendMessageHandler}>
+          <div className={classes.controls}>
+            <div className={classes.control}>
+              <label htmlFor="email">Your email</label>
+              <input
+                type="email"
+                id="email"
+                required
+                value={enteredEmail}
+                onChange={(e) => setEnteredEmail(e.target.value)}
+              />
+            </div>
+            <div className={classes.control}>
+              <label htmlFor="name">Your name</label>
+              <input
+                type="name"
+                id="name"
+                required
+                value={enteredName}
+                onChange={(e) => setEnteredName(e.target.value)}
+              />
+            </div>
           </div>
           <div className={classes.control}>
-            <label htmlFor="name">Your name</label>
-            <input
-              type="name"
-              id="name"
-              required
-              value={enteredName}
-              onChange={(e) => setEnteredName(e.target.value)}
-            />
+            <label htmlFor="message">Your Message</label>
+            <textarea
+              id="message"
+              rows="5"
+              value={enteredMessage}
+              onChange={(e) => setEnteredMessage(e.target.value)}
+            ></textarea>
           </div>
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="message">Your Message</label>
-          <textarea
-            id="message"
-            rows="5"
-            value={enteredMessage}
-            onChange={(e) => setEnteredMessage(e.target.value)}
-          ></textarea>
-        </div>
-        <div className={classes.actions}>
-          <button>Send Message</button>
-        </div>
-      </form>
-    </section>
+          <div className={classes.actions}>
+            <button>Send Message</button>
+          </div>
+        </form>
+      </section>
+    </>
   );
 };
 

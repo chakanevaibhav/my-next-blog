40533d25b1c0206835627b33404f492ee3f3cd30
Vaibhav Chakane <vaibhavchakane@Vaibhavs-MacBook-Air.local>
2023-11-02 18:52:26 +0530

    updating message

--- components/home/hero.js
+++ components/home/hero.js
@@ -1,18 +1,25 @@
-import classes from './hero.module.scss'
-import Image from 'next/image'
+import classes from "./hero.module.scss";
+import Image from "next/image";
 
 const Hero = () => {
   return (
     <section className={classes.hero}>
-        <div className={classes.image}>
-            <Image src="/images/site/corporate-guy-profile-image.avif" alt="An Image showing Me" width={360} height={360} />
-        </div>
-        <h1>Hi, I am Vaibhav</h1>
-        <p>
-            I am a web developer specialized in web development using frontend framework like next.js, react.js along with Javascript, HTML, CSS etc.
-        </p>
+      <div className={classes.image}>
+        <Image
+          src="/images/site/corporate-guy-profile-image.avif"
+          alt="An Image showing Me"
+          width={360}
+          height={360}
+        />
+      </div>
+      <h1>Hi, I am Vaibhav</h1>
+      <p>
+        I specialize in web development, focusing on frontend frameworks such
+        as Next.js and React.js, in addition to utilizing JavaScript, HTML, CSS,
+        and more.
+      </p>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;

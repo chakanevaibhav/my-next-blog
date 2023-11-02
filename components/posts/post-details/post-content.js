f2794127449f8e0c0badf44e1f3ff70d09d22968
Vaibhav Chakane <vaibhavchakane@Vaibhavs-MacBook-Air.local>
2023-11-02 15:16:47 +0530

    fetch data for the single post component

--- components/posts/post-details/post-content.js
+++ components/posts/post-details/post-content.js
@@ -3,20 +3,13 @@ import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 import ReactMarkdown from "react-markdown";
 
-const DUMMY_POST = {
-  slug: "getting-started-with-nextjs",
-  title: "Getting Started with NextJS",
-  image: "getting-started-with-nextjs.png",
-  date: "2023-03-10",
-  content: "# This is my post which has all the details",
-};
-
-const PostContent = () => {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+const PostContent = (props) => {
+  const { post } = props;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 };
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,8 +1,30 @@
 import React from "react";
 import PostContent from "../../components/posts/post-details/post-content";
+import { getPostData, getPostsFiles } from "../../lib/posts-util";
 
-const PostDetails = () => {
-  return <PostContent />;
+const PostDetails = (props) => {
+  return <PostContent post={props.post} />;
+};
+
+export const getStaticProps = (context) => {
+  const { params } = context;
+  const { slug } = params;
+  const postData = getPostData(slug);
+  return {
+    props: {
+      post: postData,
+    },
+    revalidate: 600,
+  };
+};
+
+export const getStaticPaths = () => {
+  const postFileNames = getPostsFiles();
+  const slugs = postFileNames.map((fileName) => fileName.replace(/\.md$/, ""));
+  return {
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    fallback: false,
+  };
 };
 
 export default PostDetails;
